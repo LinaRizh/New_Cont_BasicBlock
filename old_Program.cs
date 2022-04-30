b852755da61fc27f026ea41e093a59a5fb165005
PoLina <Polinochka_-95@mail.ru>
2022-04-30 13:48:23 +0300

    old program

---
+++ old_Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+string [] Array = new string [] {"привет","111","кот","солнце","2","сон"};
+int j = 0;
+string s = String.Empty;
+string [] new_array = new string [Array.Length];
+for (int i =0; i<Array.Length; i++)
+{
+    if (Array[i].Length<4)
+    {
+        new_array[j] = Array[i];
+        Write($"{new_array[j]} ");     // если не удалять эту строку, код сработает корректно при любом наборе данных
+    }
+}
+WriteLine();
